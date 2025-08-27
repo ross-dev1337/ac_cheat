4b432cab7ad07f11a064ba15a4f5ebc5b74441e6
ross-dev1337 <ross.buchanan13@gmail.com>
2025-08-27 21:25:53 +0100

    update global variables

--- ac_cheat/src/engine/globals.cpp
+++ ac_cheat/src/engine/globals.cpp
@@ -4,6 +4,12 @@ namespace globals::vars {
 	HWND window = 0;
 	bool run = false;
 	WNDPROC org_wndproc = nullptr; 
+	uint32_t module_base = 0;
+
+	namespace engine {
+		uint32_t player_count = 0;
+		float* view_matrix = nullptr;
+	}
 
 	namespace menu_settings {
 		bool menu_open = false;
@@ -15,5 +21,15 @@ namespace globals::vars {
 		namespace visuals {
 			bool snap_lines, names_distance, box;
 		}
+	}
+
+	namespace imgui_colors {
+		ImVec4 blue = { 0.f, .3f, .9f, 1 };
+		ImVec4 light_blue = { .1f, .5f, 1, 1 };
+		ImVec4 red = { 1, 0, 0, 1 };
+		ImVec4 white = { 1, 1, 1, 1 };
+		ImVec4 black = { 0.f, 0.f, 0.f, 0.5f };
+		ImVec4 green = { 0, .8f, .1f, 1 };
+		ImVec4 yellow = { .9f, .9f, .1f, 1 };
 	}
 }
--- ac_cheat/src/engine/globals.h
+++ ac_cheat/src/engine/globals.h
@@ -4,6 +4,12 @@ namespace globals::vars {
 	extern HWND window;
 	extern bool run;
 	extern WNDPROC org_wndproc;
+	extern uint32_t module_base;
+
+	namespace engine {
+		extern uint32_t player_count;
+		extern float* view_matrix; 
+	}
 
 	namespace menu_settings {
 		extern bool menu_open;
@@ -16,4 +22,20 @@ namespace globals::vars {
 			extern bool snap_lines, names_distance, box; 
 		}
 	}
+
+	namespace imgui_colors {
+		extern ImVec4 blue;
+		extern ImVec4 light_blue;
+		extern ImVec4 red;
+		extern ImVec4 white;
+		extern ImVec4 black;
+		extern ImVec4 green;
+		extern ImVec4 yellow;
+	}
+
+	enum align {
+		ALIGN_X_LEFT = 1 << 1,
+		ALIGN_X_CENTER = 1 << 2,
+		ALIGN_X_RIGHT = 1 << 3,
+	};
 }
